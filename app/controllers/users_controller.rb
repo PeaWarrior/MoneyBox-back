5f870fde9e318564dad97a761e83699226a4d335
PeaWarrior <jacksonchen171@gmail.com>
2020-09-17 13:22:05 -0400

    added authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
+    before_action :authenticate, only: [:autologin]
 
     def create
         user = User.create(user_params)
         if user.valid?
-            token = JWT.encode({ user_id: user.id }, 'so_secret', 'HS256')
+            token = encode_token({ user_id: user.id })
 
             render json: { user: UserSerializer.new(user), token: token }, status: :created
         else 
@@ -14,29 +15,16 @@ class UsersController < ApplicationController
     def login
         user = User.find_by(username: user_params[:username])
         if user && user.authenticate(user_params[:password])
-            token = JWT.encode({ user_id: user.id }, 'so_secret', 'HS256')
-            
-            render json: { user: UserSerializer.new(user), token: token }, status: :authorized
+            token = encode_token({ user_id: user.id })
+
+            render json: { user: UserSerializer.new(user), token: token }
         else
             render json: { error: 'Invalid username or password' }, status: :unauthorized
         end
     end
 
     def autologin
-        auth_header = request.headers['Authorization']
-        token = auth_header.split(' ')[1]
-
-        decoded_token = JWT.decode(token, "so_secret", true, {algorithm: 'HS256'})
-
-        user_id = decoded_token[0]["user_id"]
-        user = User.find_by()
-
-        if user
-            render json: { user: UserSerializer.new(user) }, status: :authorized
-        else
-            render json: { error: 'Not logged in' }, status: :unauthorized
-        end
-
+        render json: { user: UserSerializer.new(@current_user) }
     end
 
     private
